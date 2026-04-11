2fb58d0c11fc24993c17a6c4ba1b6b9862c9af7d
danielarturo661-hash <danielarturo661@gmail.com>
2026-04-10 20:51:09 -0600

    Fix bucket name in list_buckets function

--- automation-python/s3_list.py
+++ automation-python/s3_list.py
@@ -2,5 +2,5 @@ import boto3
 s3 = boto3.client('s3')
 def list_buckets():
     resp = s3.list_buckets()
-    for b in resp['Buckets']: print(f'Bucket: {b['Name']}')
+    for b in resp['Buckets']: print(f'Bucket: {b['stf-proyecto-devops-ale-data']}')
 if __name__ == '__main__': list_buckets()
